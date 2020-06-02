c099e418a0eccbafda655b26804627f01cb90f81
eslam <eslamelkholy5325762@gmail.com>
2020-06-02 20:56:58 +0200

    Events Model

--- server/app.js
+++ server/app.js
@@ -1,17 +1,19 @@
-const grapqlHTPP = require("express-graphql");
-const schema = require("./schema/schema");
+const grapqlHttp = require("express-graphql");
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
+const schema = require("./schema/schema");
 
 const db = mongoose.connect("mongodb://localhost:27017/GraphQL");
 
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/graphql', grapqlHTPP({
+app.use('/graphql', grapqlHttp({
     schema,
+    rootValue:{ },
     graphiql:true
 }));
 
